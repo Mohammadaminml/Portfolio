1eee0f933f8f5d61674f61e92a6470e29607d47e
Mohammad Amin Mollakazemiha <mohammadaminml@icloud.com>
2026-07-25 23:54:34 +0330

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/Portfolio/",
+  build: {
+    emptyOutDir: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
